7cec72c27732fc4c3bb379a4127a4e24c4ce1fb1
AuctorisMors <GitHub@Baerthe.com>
2025-09-24 17:07:43 -0700

    Refactor player movement logic for clarity and maintainability

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Player : Area2D
 {
+	[Signal]
+	public delegate void HitEventHandler();
 	[Export]
 	public int Speed { get; set; } = 400;
 	[Export]
@@ -12,16 +14,37 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//Hide();
 		_screenSize = GetViewportRect().Size;
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		var velocity = Vector2.Zero;
-		if (Input.IsActionPressed("move_up")) velocity.Y -= 1;
-		if (Input.IsActionPressed("move_down")) velocity.Y += 1;
-		if (Input.IsActionPressed("move_left")) velocity.X -= 1;
-		if (Input.IsActionPressed("move_right")) velocity.X += 1;
+		//What direction is the player going?
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+			playerDirection = PlayerDirection.Up;
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+			playerDirection = PlayerDirection.Down;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+			playerDirection = PlayerDirection.Left;
+		}
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+			playerDirection = PlayerDirection.Right;
+		}
+		// If the player is going two directions, ie diagonal, set special state.
+		if (velocity.Y != 0 && velocity.X != 0)
+			playerDirection = PlayerDirection.Diagonal;
 		if (velocity.Length() > 0)
 		{
 			velocity = velocity.Normalized() * Speed;
@@ -29,22 +52,10 @@ public partial class Player : Area2D
 		}
 		else
 			sprite.Stop();
+		// Move the player.
 		Position += velocity * (float)delta;
 		Position = new Vector2(x: Mathf.Clamp(Position.X, 0, _screenSize.X), y: Mathf.Clamp(Position.Y, 0, _screenSize.Y));
 		sprite.FlipV = false; // Make sure we never flip vertically
-		// Which direction is the player going?
-		if (velocity.Y != 0 && velocity.X != 0)
-			playerDirection = PlayerDirection.Diagonal;
-		else if (velocity.Y != 0)
-			if (velocity.Normalized().Y == 1)
-				playerDirection = PlayerDirection.Down;
-			else
-				playerDirection = PlayerDirection.Up;
-		else if (velocity.X != 0)
-			if (sprite.FlipH = velocity.X < 0)
-				playerDirection = PlayerDirection.Left;
-			else
-				playerDirection = PlayerDirection.Right;
 		// Set animation based on direction
 		switch (playerDirection)
 		{
@@ -59,10 +70,11 @@ public partial class Player : Area2D
 				sprite.FlipH = velocity.X < 0;
 				break;
 			case PlayerDirection.Left:
-				sprite.Animation = "Right";
 				sprite.FlipH = true;
+				sprite.Animation = "Right";
 				break;
 			case PlayerDirection.Right:
+				sprite.FlipH = false;
 				sprite.Animation = "Right";
 				break;
 			default:
